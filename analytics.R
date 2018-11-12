3dfbd4299b288044f744b860ec4e8e4d031da2f5
Vadim Khaitov <polydora@rambler.ru>
2018-11-12 18:22:39 +0300

    открыл страницу про экзамен

---
+++ analytics.R
@@ -0,0 +1,19 @@
+# Анализ попыток студентов 
+
+trial <- read.csv("data/course-32065-submissions.csv", header = T)
+
+
+trial$attempt <- format(as.POSIXct((trial$attempt_time), origin = "1970-01-01", tz = "UTC"), "%d-%m %H:%M")
+
+trial$submission <- format(as.POSIXct((trial$submission_time), origin = "1970-01-01", tz = "UTC"), "%d-%m %H:%M")
+
+
+tail(trial[trial$first_name == "Смутин", ])
+
+
+write.csv(trial[trial$first_name == "Смутин", ], "data/Smutin.csv")
+
+write.csv(trial[trial$last_name == "Тишкова", ], "data/Tishkova.csv")
+
+
+write.csv(trial[trial$last_name == "Воскресенсксая", ], "data/Voskresenskaya.csv")

a8fca00b3b5452f5cad2be9d43c8ad44b3c88609
Vadim Khaitov <polydora@rambler.ru>
2018-11-13 17:35:44 +0300

    Update analytics.R

--- analytics.R
+++ analytics.R
@@ -8,6 +8,10 @@ trial$attempt <- format(as.POSIXct((trial$attempt_time), origin = "1970-01-01", 
 trial$submission <- format(as.POSIXct((trial$submission_time), origin = "1970-01-01", tz = "UTC"), "%d-%m %H:%M")
 
 
+library(ggplot2)
+
+ggplot(trial, aes(x = submission_time )) + geom_histogram(binwidth = 1)
+
 tail(trial[trial$first_name == "Смутин", ])
 
 

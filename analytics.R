7e5e4d5035eed7b55ca692583da74560fc663a9b
Vadim Khaitov <polydora@rambler.ru>
2019-10-27 19:28:31 +0300

    Вывесил суммарные баллы

--- analytics.R
+++ analytics.R
@@ -2,23 +2,38 @@
 
 trial <- read.csv("data/course-32065-submissions.csv", header = T)
 
+class <- read.csv("data/class.csv")
+
+class <- class[, c("Name", "ID", "Group")]
+
+trial <- merge(class, trial, by = "ID")
+
+
+
 
 trial$attempt <- format(as.POSIXct((trial$attempt_time), origin = "1970-01-01", tz = "UTC"), "%d-%m %H:%M")
 
 trial$submission <- format(as.POSIXct((trial$submission_time), origin = "1970-01-01", tz = "UTC"), "%d-%m %H:%M")
 
 
-library(ggplot2)
 
-ggplot(trial, aes(x = submission_time )) + geom_histogram(binwidth = 1)
 
-tail(trial[trial$last_name == "Сосновский", ])
+tail(trial[trial$last_name == "Тарасова", ])
 
 write.csv(trial, "data/all_attempts.csv")
 
-write.csv(trial[trial$first_name == "Смутин", ], "data/Smutin.csv")
-
-write.csv(trial[trial$last_name == "Тишкова", ], "data/Tishkova.csv")
+write.csv(trial[trial$last_name == "Тарасова", ], "data/Tarasova.csv")
 
 
-write.csv(trial[trial$last_name == "Воскресенсксая", ], "data/Voskresenskaya.csv")
+###############################
+
+contr_trial <- read.csv("data/Control1_attempts.csv", header = T)
+
+contr_trial$submission_time <- as.POSIXct(contr_trial$submission,format = "%d-%m %H:%M")
+
+
+ggplot(contr_trial, aes(x = Group, y = submission_time)) + geom_boxplot() + labs(x = "Группа", y = "Час, когда начали работу")
+
+
+
+
